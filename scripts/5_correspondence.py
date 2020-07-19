c4299d2902f05bad73c241bbb90e79241d3d0bec
Lee Junsung <leejunsung@Lees-MacBook-Air.local>
2020-07-19 11:09:21 +0900

    Palaung phylogeny, first pass

---
+++ scripts/5_correspondence.py
@@ -0,0 +1,36 @@
+from lingrex.copar import CoPaR
+from sys import argv
+
+if 'all' in argv:
+    fname='../output/A_Deepadung_'
+else:
+    fname='../output/D_Deepadung_'
+
+cop = CoPaR(
+        fname+'crossids.tsv',
+        ref='crossids',
+        fuzzy=True,
+        segments='tokens'
+        )
+cop.get_sites(minrefs=3)  #part where change was made.,structure='structure' deleted
+cop.cluster_sites()
+cop.sites_to_pattern()
+cop.add_patterns()
+cop.write_patterns(fname+'all_patterns.tsv')
+cop.output('tsv', filename=fname+'patterns', prettify=False)
+
+# statistics 
+sps=['i','m','n','c','t']
+
+total_correspondence_sets = len(cop.clusters)
+print('{0}: {1}'.format('The total sound correspondence cluster sets', total_correspondence_sets))
+
+print('The number of regular correspondence sets in each position')
+for sp in sps:
+    t = [x[1] for x, y in cop.clusters.items() if len(y)>1 and x[0] ==sp]
+    print('{0}: {1}'.format(sp, len(t)))
+
+print('The number of singletons in each position ')
+for sp in sps:
+    t = [x[1] for x, y in cop.clusters.items() if len(y)==1 and x[0] ==sp]
+    print('{0}: {1}'.format(sp, len(t)))
---
+++ scripts/6_phylogeny.py
@@ -0,0 +1,27 @@
+from lingpy.compare.partial import Partial
+from lingpy.convert.plot import plot_tree
+from sys import argv
+from clldutils.text import strip_brackets, split_text
+from collections import defaultdict
+from lingpy import basictypes
+
+if 'all' in argv:
+    fname='../output/A_Deepadung_'
+else:
+    fname='../output/D_Deepadung_'
+
+part = Partial(fname+'crossids.tsv')
+part.add_cognate_ids('crossids', 'crossid', idtype='strict')
+part.add_entries('cog', 'crossid,concept', lambda x, y: str(x[y[0]])+x[y[1]])
+part.renumber('cog')
+
+part.calculate('distance', ref='cogid')
+part.calculate('tree', tree_calc='neighbor')
+
+part.output('dst', filename=fname+'distance')
+part.output('tre', filename=fname+'tree')
+
+if 'plot' in argv:
+    plot_tree(str(part.tree), degree=350, filename=fname+'tree')
+
+
